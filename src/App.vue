c6a439a2d0974a6adab1a5efca5e29842cc42c80
ludovicaghemo <ludovica.aghemo@gmail.com>
2023-12-04 14:00:18 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+//importare componenti
+import { store } from "./store.js";
+
+//dichiarare componenti
+export default {
+  data() {
+    return {
+      store: store
+    };
+  },
+  created() {
+  },
+  components: {},
+  methods: {
+  },
+};
+</script>
+
+<template>
+  <!-- utilizzare componenti-->
+  <h1>Help</h1>
+</template>
+
+<style lang="scss">
+@use "./style/general.scss";
+@import "@fortawesome/fontawesome-free/css/all.css"
+</style>
